aac29b716e71f04087c71b85b2ac91740ff2ca30
Jim Tennant <jtennant@applied.co>
2026-03-30 15:21:18 +0000

    Pushing March updates

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -10,8 +10,8 @@ fi
 curl -sS https://webi.sh/shfmt | sh &>/dev/null
 
 # Add OMZ plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting 2>/dev/null || true
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions 2>/dev/null || true
 sed -i -E "s/^(plugins=\()(git)(\))/\1\2 zsh-syntax-highlighting zsh-autosuggestions\3/" ~/.zshrc
 
 # Avoid git log use less
